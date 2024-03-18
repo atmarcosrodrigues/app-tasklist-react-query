ddd4408158d03de6947ceb4f5b6a4629498a7106
atmarcosrodrigues <antonio.rodrigues.ct@gmail.com>
2024-03-18 17:14:09 -0300

    feat: Add tasklist request/update funcionality

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
-
+import getTasks from "../../services/getTasks";
+import updateTask from "../../services/updateTask";
 import "./styles.css";
 
 interface Task {
@@ -10,7 +11,49 @@ interface Task {
 }
 
 function TaskList({ queryClient }: { queryClient: QueryClient }) {
-  return <div className="app-container"></div>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["taskList"],
+    queryFn: () => getTasks(),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (task: Task) => {
+      return updateTask(task.id, task.completed);
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["taskList"], (currentData: Task[]) =>
+        currentData.map((task: Task) => (task.id === data.id ? data : task))
+      );
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  if (isPending) return <div className="message"> Loading....</div>;
+
+  if (error)
+    return (
+      <div className="message">"An error has occurred: " + error.message;</div>
+    );
+
+  return (
+    <div className="app-container">
+      <div className="todos">
+        {data.map((task: Task) => (
+          <div
+            onClick={() =>
+              mutation.mutate({ id: task.id, completed: !task.completed })
+            }
+            className={`todo ${task.completed && "todo-completed"}`}
+            key={task.id}
+          >
+            {task.todo}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TaskList;
